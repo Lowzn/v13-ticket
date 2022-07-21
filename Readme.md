90d82fb1a9894ab3ec7232e1b1dcad906618d064
novabotlist <93944142+moderasyonbo@users.noreply.github.com>
2022-07-21 12:39:20 +0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,1 @@
+![image](https://user-images.githubusercontent.com/93944142/180084915-8e281915-81cd-4364-8572-a008e245e22a.png)
---
+++ index.js
@@ -0,0 +1,28 @@
+const {Client, Collection, Intents} = require('discord.js');
+const {readdirSync } = require('fs')
+const client = new Client({ intents: 32767 })
+
+const config = require('./config.json');
+
+client.login(config.token);
+client.commands = new Collection();
+
+
+
+const commandFiles = readdirSync('./command').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./command/${file}`);
+	client.commands.set(command.name, command);
+	}
+
+
+
+const eventFiles = readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(client, ...args));
+	} else {
+		client.on(event.name, (...args) => event.execute( client, ...args));
+	}
+}
